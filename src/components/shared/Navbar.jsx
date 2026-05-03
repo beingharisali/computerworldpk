de2481d663e2c1730890c1d44cf2ae89c2966e09
beingharisali <theharis99@gmail.com>
2026-05-03 14:19:37 +0500

    updated logo

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -6,7 +6,7 @@ function Navbar() {
     <header class="text-gray-600 body-font">
       <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-          <svg
+          {/* <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             stroke="currentColor"
@@ -17,9 +17,9 @@ function Navbar() {
             viewBox="0 0 24 24"
           >
             <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-          </svg>
-          <Link to="/" class="ml-3 text-xl">
-            Ecommerce
+          </svg> */}
+          <Link to="/">
+            <img src="/logo.png" alt="" />
           </Link>
         </a>
         <nav class="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
